83f0c8b7f63ba70fa9f8bff6fbbf093c32ce9298
test <test168168@protonmail.com>
2020-12-14 14:45:13 +0800

    update 注册 欢迎页面

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->nullable();
-            $table->string('user_name')->nullable()->unique()->index();
+            $table->string('username')->nullable()->unique()->index();
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
